fcc55ef280f27f9e0b95f332e7e7c3dca73fbdc5
LucaGoubelle <luca.goubelle97@gmail.com>
2025-07-07 23:37:30 +0200

    Start implementing last face handler

--- solver2x2/handlers/last_face_handler.lua
+++ solver2x2/handlers/last_face_handler.lua
@@ -1,14 +1,40 @@
+require "luacube.move.mover"
 -- last face handler
 LastFaceHandler={}
 
 function LastFaceHandler:new()
-    local obj = {}
+    local obj = {
+        mover = Mover:new()
+    }
     setmetatable(obj, self)
     self.__index = self
     return obj
 end
 
-function LastFaceHandler:handle(cube)
+function LastFaceHandler:handleOLL(cube)
     -- todo: implement this
+    return cube
+end
+
+function LastFaceHandler:handlePLL(cube)
+    -- todo: implement this
+    return cube
+end
+
+function LastFaceHandler:handleAUF(cube)
+    local color = cube.front[1][1]
+    local hmap = {
+        red = "U'",
+        orange = "U",
+        green = "U2"
+    }
+    cube = self.mover:multiMoves(cube, hmap[color] or "???")
+    return cube
+end
+
+function LastFaceHandler:handle(cube)
+    cube = self:handleOLL(cube)
+    cube = self:handlePLL(cube)
+    cube = self:handleAUF(cube)
     return cube
 end
