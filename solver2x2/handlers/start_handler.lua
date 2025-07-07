b4e66a2762bce86df4f9bb30c42865d989e8047d
LucaGoubelle <luca.goubelle97@gmail.com>
2025-07-07 22:45:00 +0200

    Implement start handler + wbr processor

--- solver2x2/handlers/start_handler.lua
+++ solver2x2/handlers/start_handler.lua
@@ -1,14 +1,36 @@
+require "solver_helpers.scanners.cube_2x2_scanner"
+require "solver_helpers.seekers.corner_2_seeker"
+require "solver2x2.processors.first_corners.wbr_processor"
+require "luacube.move.mover"
 -- start handler
 StartHandler={}
 
 function StartHandler:new()
-    local obj = {}
+    local obj = {
+        scanner = Cube2x2Scanner:new(),
+        seeker = Corner2Seeker:new(),
+        wbrProcessor = WBRProcessor:new(),
+        mover = Mover:new()
+    }
     setmetatable(obj, self)
     self.__index = self
     return obj
 end
 
-function StartHandler:handle(cube)
-    -- todo: implement this
+function StartHandler:_handleFirstCorner(cube)
+    local cornerInfo = self.seeker:seekCorner(cube, {
+        "white_blue_red", "white_red_blue",
+        "blue_white_red", "blue_red_white",
+        "red_white_blue", "red_blue_white"
+    })
+    local sequence = self.wbrProcessor:process(cornerInfo)
+    cube = self.mover:multiMoves(cube, sequence)
     return cube
 end
+
+function StartHandler:handle(cube)
+    local corner = self.scanner:scanCorner(cube, "down_front_right")
+    if corner == "white_blue_red" then return cube end -- nothing to do
+    return self:_handleFirstCorner(cube)
+end
+
--- solver2x2/processors/first_corners/wbr_processor.lua
+++ solver2x2/processors/first_corners/wbr_processor.lua
@@ -1,1 +1,48 @@
--- todo: implement this
+-- white blue red corner processor
+WBRProcessor={}
+
+function WBRProcessor:new()
+    local obj = {
+        data={
+            up_front_right__white_red_blue = "U' R2",
+            up_front_right__blue_white_red = "R'",
+            up_front_right__red_blue_white = "F",
+
+            up_front_left__white_blue_red = "F2",
+            up_front_left__red_white_blue = "U' F",
+            up_front_left__blue_red_white = "U' R'",
+
+            up_back_right__white_blue_red = "R2",
+            up_back_right__red_white_blue = "U F",
+            up_back_right__blue_red_white = "U R'",
+
+            up_back_left__white_red_blue = "U R2",
+            up_back_left__blue_white_red = "U2 R'",
+            up_back_left__red_blue_white = "U2 F",
+
+
+            down_front_left__white_red_blue = "D",
+            down_front_left__blue_white_red = "L D2",
+            down_front_left__red_blue_white = "F'",
+
+            down_back_left__white_blue_red = "D2",
+            down_back_left__red_white_blue = "L' D",
+            down_back_left__blue_red_white = "L' F'",
+
+            down_back_right__white_red_blue = "D'",
+            down_back_right__blue_white_red = "R",
+            down_back_right__red_blue_white = "R2 F",
+            
+            down_front_right__red_white_blue = "R' D'",
+            down_front_right__blue_red_white = "F D"
+        }
+    }
+    setmetatable(obj, self)
+    self.__index = self
+    return obj
+end
+
+function WBRProcessor:process(inputData)
+    local result = data[inputData] or "???"
+    return result
+end
