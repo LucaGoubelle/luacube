6032b6cb7ecd4b46a94770f6deab54a79c26837d
LucaGoubelle <luca.goubelle97@gmail.com>
2025-07-07 23:31:21 +0200

    Implement first corner handler

--- solver2x2/handlers/first_corners_handler.lua
+++ solver2x2/handlers/first_corners_handler.lua
@@ -1,14 +1,63 @@
+require "luacube.move.mover"
+require "solver_helpers.seekers.corner_2_seeker"
+
+require "solver2x2.processors.first_corners.wgr_processor"
+require "solver2x2.processors.first_corners.wgo_processor"
+require "solver2x2.processors.first_corners.wbo_processor"
+
 -- first corner handler
 FirstCornerHandler={}
 
 function FirstCornerHandler:new()
-    local obj = {}
+    local obj = {
+        mover = Mover:new(),
+        seeker = Corner2Seeker:new(),
+        -- processors
+        wgrProc = WGRProcessor:new(),
+        wgoProc = WGOProcessor:new(),
+        wboProc = WBOProcessor:new()
+    }
     setmetatable(obj, self)
     self.__index = self
     return obj
 end
 
+function FirstCornerHandler:handleWGR(cube)
+    local corner = self.seeker:seekCorner(cube, {
+        "white_green_red","white_red_green",
+        "green_white_red","green_red_white",
+        "red_white_green","red_green_white"
+    })
+    local sequence = self.wgrProc:process(corner)
+    cube = self.mover:multiMoves(cube, sequence)
+    return cube
+end
+
+function FirstCornerHandler:handleWGO(cube)
+    local corner = self.seeker:seekCorner(cube, {
+        "white_green_orange","white_orange_green",
+        "green_white_orange","green_orange_white",
+        "orange_white_green","orange_green_white"
+    })
+    local sequence = self.wgoProc:process(corner)
+    cube = self.mover:multiMoves(cube, sequence)
+    return cube
+end
+
+function FirstCornerHandler:handleWBO(cube)
+    local corner = self.seeker:seekCorner(cube, {
+        "white_blue_orange","white_orange_blue",
+        "blue_white_orange","blue_orange_white",
+        "orange_white_blue","orange_blue_white"
+    })
+    local sequence = self.wboProc:process(corner)
+    cube = self.mover:multiMoves(cube, sequence)
+    return cube
+end
+
 function FirstCornerHandler:handle(cube)
-    -- todo: implement this
+    cube = self:handleWGR(cube)
+    cube = self:handleWGO(cube)
+    cube = self:handleWBO(cube)
     return cube
 end
