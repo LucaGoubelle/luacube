66bbeaeff0bae2db26d1b90b2197c195a9383eb1
LucaGoubelle <luca.goubelle97@gmail.com>
2025-07-08 09:46:38 +0200

    Add PLLs cases in PLL processor

--- solver2x2/processors/last_face/pll_processor.lua
+++ solver2x2/processors/last_face/pll_processor.lua
@@ -7,7 +7,18 @@ PLLProcessor={}
 
 function PLLProcessor:new()
     local obj = {
-        data={}
+        data={
+            -- T cases
+            BR_GB_RG_OO = ca.T_PERM,
+            RG_0R_GO_BB = ca.T_PERM,
+            GO_BG_OB_RR = ca.T_PERM,
+            OB_RO_BR_GG = ca.T_PERM,
+
+            -- Y cases
+            BG_OR_GB_RO = ca.Y_PERM,
+            GB_RO_BG_OR = ca.Y_PERM,
+            RO_BG_OR_GB = ca.Y_PERM
+        }
     }
     setmetatable(obj, self)
     self.__index = self
