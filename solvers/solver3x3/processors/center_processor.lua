379e307eca38e810fe3b026a081b446fec206188
LucaGoubelle <luca.goubelle97@gmail.com>
2026-03-06 08:32:51 +0100

    Add processor mother table in solver3x3

--- solvers/solver3x3/processors/center_processor.lua
+++ solvers/solver3x3/processors/center_processor.lua
@@ -1,20 +1,15 @@
--- center processor
-CenterProcessor={}
+-- center 
+require "solvers.solver3x3.processors.processor"
+
+CenterProcessor=setmetatable({}, {__index=Processor})
+CenterProcessor.__index = CenterProcessor
 
 function CenterProcessor:new()
-    local obj = {
-        data = {
-            white_blue="z2",
-            white_green="x2",
-            yellow_green="y2"
-        }
+    local obj = setmetatable(Processor:new(), self)
+    obj.data = {
+        white_blue="z2",
+        white_green="x2",
+        yellow_green="y2"
     }
-    setmetatable(obj, self)
-    self.__index = self
     return obj
-end
-
-function CenterProcessor:process(inputData)
-    local result = self.data[inputData] or "???"
-    return result
 end
